d094c513ab5177a501a9a94134f0aac36c4b7fc1
Paolo Carta <pcarta@redhat.com>
2020-03-06 11:25:42 +0100

    testing configurations

--- applications/guestbook-ui/src/main/java/com/example/guestbook/HelloworldUiController.java
+++ applications/guestbook-ui/src/main/java/com/example/guestbook/HelloworldUiController.java
@@ -3,8 +3,10 @@ package com.example.guestbook;
 
 import java.util.Map;
 
+import org.infinispan.client.hotrod.RemoteCache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +15,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
-@SessionAttributes("name")
+//@SessionAttributes("name")
 public class HelloworldUiController {
 
   private final static Logger logger = LoggerFactory.getLogger(HelloworldUiController.class);
 
+  @Autowired
+  private RemoteCache<String, String> defaultCache;
 
   private final HelloworldService helloworldService;
   private final GuestbookService guestbookService;
@@ -34,6 +38,8 @@ public class HelloworldUiController {
   public String index(Model model) {
 
     logger.info("Inside get index method");
+
+//    defaultCache.getName();
 
     if (model.containsAttribute("name")) {
       String name = (String) model.asMap().get("name");
