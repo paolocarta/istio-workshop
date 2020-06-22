ab964adda6f98d254dd5501900ec73199abf12bb
Paolo Carta <ing.pcarta@gmail.com>
2020-06-22 20:15:35 +0200

    extending cleanup

--- istio-rules/11c-delete-helloworld-v2.sh
+++ istio-rules/11c-delete-helloworld-v2.sh
@@ -1,2 +1,3 @@
 oc delete -f kubernetes-v2/permissive/helloworld-deployment-v2.yaml
 oc delete destinationrules.networking.istio.io helloworld-service
+oc delete virtualservices.networking.istio.io helloworld-service
